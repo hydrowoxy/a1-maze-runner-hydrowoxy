12f293cd66bb438a8b0f62aa458bc2dda4f053c8
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-01-19 10:32:14 -0500

    added classes to start on skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -9,27 +9,19 @@ import org.apache.commons.cli.*;
 
 public class Main {
 
-    // Logger for Main
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
 
-        // Create options object for command-line options
         Options options = new Options();
-        // Adds a command line option to the Options object
-        // Short form -i, long form --i, expects a value, description is Input file
+
         options.addOption("i", "input", true, "Input file");
 
-        // DefaultParser is an implementation of CommandLineParser to parse command-line arguments
         CommandLineParser parser = new DefaultParser();
         try {
-            // Parse method to parse command line arguments based on the options we set
-            // Obtains command-line object cmd
             CommandLine cmd = parser.parse(options, args);
 
-            // Check if i or input is in that command-line object
             if (cmd.hasOption("i") || cmd.hasOption("input")) {
-                // Retrieve associeted value i.e. input file path
                 String inputFile = cmd.getOptionValue("i", cmd.getOptionValue("input"));
                 
                 logger.info("** Starting Maze Runner");
