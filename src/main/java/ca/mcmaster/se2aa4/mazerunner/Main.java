221691a09d22b06db79e1ba0feacfbea58336ea6
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-01-20 13:01:06 -0500

    accepting -p flag (and --path) as a command line option (close #3)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -12,25 +12,29 @@ public class Main {
     public static void main(String[] args) {
 
         Options options = new Options();
-        options.addOption("i", "input", true, "Input file");
+        options.addOption("i", "input", true, "Input maze file");
+        options.addOption("p", "path", false, "Input path (to be verified)");
 
         CommandLineParser parser = new DefaultParser();
 
         try {
+
             CommandLine cmd = parser.parse(options, args);
 
             if (cmd.hasOption("i") || cmd.hasOption("input")) {
                 String inputFile = cmd.getOptionValue("i", cmd.getOptionValue("input"));
-                
+
                 logger.info("** Starting Maze Runner");
 
                 Maze maze = new Maze();
                 maze.read(inputFile);
                 maze.disp();
 
-                Path path = new Path(maze);
-                path.compute();
-                path.disp();
+                if (cmd.hasOption("p") || cmd.hasOption("path")) {
+                    Path path = new Path(maze);
+                    path.compute();
+                    path.disp();
+                }
 
                 logger.info("** End of MazeRunner");
 
@@ -39,6 +43,7 @@ public class Main {
         } catch (ParseException e) {
             System.err.println("Error parsing command line options: " + e.getMessage());
         }
+
     }
 }
 
