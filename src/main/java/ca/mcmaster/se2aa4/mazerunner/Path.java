37ef9646bb7b728f19b74fb8d465a6ebae430882
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-01-22 14:57:49 -0500

    Added abstract parent class/implemented better code structure to support adding algorithms (close #22)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -2,21 +2,12 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.awt.Point;
 
-public class Path {
+public class Path extends Traversal {
 
     private String inputPath;
-    private char[][] mazeArr;
-    private Point start;
-    private int endCol;
 
-    private char[] dir = { 'N', 'E', 'S', 'W' };
-    private int dirIdx = 1;
-    private char fwd = dir[dirIdx];
-    
     public Path(String inputPath, Maze maze){ //in future inherit all from traversal but also has inputpath, and takes in inputpath too
-        this.mazeArr = maze.getMazeArr();
-        this.start = maze.getStartPoint();
-        this.endCol = maze.getEndCol();
+        super(maze);
         this.inputPath = inputPath;
     }
 
@@ -48,47 +39,6 @@ public class Path {
     
     public void verify(){
         runPath(start, inputPath);
-    }
-
-    private void turnRight() {
-        dirIdx = (dirIdx + 1) % 4;
-        fwd = dir[dirIdx];
-    }
-
-    private void turnLeft() {
-        dirIdx = (dirIdx + 3) % 4;
-        fwd = dir[dirIdx];
-    }
-
-    private char toFwd(Point p) {
-        switch (fwd) {
-            case 'N':
-                return mazeArr[p.y - 1][p.x];
-            case 'E':
-                return mazeArr[p.y][p.x + 1];
-            case 'S':
-                return mazeArr[p.y + 1][p.x];
-            default:
-                return mazeArr[p.y][p.x - 1];
-        }
-    }
-
-    private void moveFwd(Point p) {
-        switch (fwd) {
-            case 'N':
-                p.y--;
-                break;
-            case 'E':
-                p.x++;
-                break;
-            case 'S':
-                p.y++;
-                break;
-            default:
-                p.x--;
-                break;
-        }
-
     }
 
 }
