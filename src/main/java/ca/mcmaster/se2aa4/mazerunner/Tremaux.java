71e6ac8d1aae9fe25b3571833b2beee4b8c6fb91
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-01-25 01:47:33 -0500

    Tremaux functional (close #20, close #24)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Tremaux.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Tremaux.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tremaux extends Algorithm {
+    Point initialPoint = new Point(start);
 
     public Tremaux(Maze maze) {
         super(maze);
@@ -13,10 +14,8 @@ public class Tremaux extends Algorithm {
 
     @Override
     protected List<Character> algorithm(Point p) {
-        System.out.println("START IS" + start);
 
         while (p.x != endCol) {
-            printMazeState();
 
             if (atJunction(p)) {
                 handleJunction(p);
@@ -25,35 +24,29 @@ public class Tremaux extends Algorithm {
             }
         }
 
-        retraceSteps(start);
+        retraceSteps(initialPoint);
         return steps;
+        
     }
 
     private void handleJunction(Point p) {
-        System.out.println("at a junction");
         turnRight();
         turnRight();
 
         if (toFwd(p) == ' ') {
             moveFwd(p);
-            System.out.println("turned and went back to entrance");
             markSingleCrossed(p);
-            printMazeState();
-            System.out.println("marked the entrance");
             turnRight();
             turnRight();
             moveFwd(p);
-            System.out.println("returned to junction");
         } else {
             turnRight();
             turnRight();
         }
 
         if (isPathBlank(p)) {
-            System.out.println("at least one path in proximity is blank");
             goBlank(p);
         } else {
-            System.out.println("no paths in front of me are blank");
             doubleCross(p);
         }
     }
@@ -63,7 +56,6 @@ public class Tremaux extends Algorithm {
     }
 
     private void goBlank(Point p) {
-        System.out.println("GOBLANK");
 
         if (toFwd(p) == ' ') {
             moveFwd(p);
@@ -76,11 +68,9 @@ public class Tremaux extends Algorithm {
         }
 
         markSingleCrossed(p);
-        printMazeState();
     }
 
     private void doubleCross(Point p) {
-        System.out.println("DOUBLECROSS");
 
         if (toBehind(p) == 'O') {
             turnRight();
@@ -97,48 +87,35 @@ public class Tremaux extends Algorithm {
         }
 
         markDoubleCrossed(p);
-        printMazeState();
     }
 
     private void markDoubleCrossed(Point p) {
-        System.out.println("MARKDOUBLECROSSED");
         mazeArr[(int) p.getY()][(int) p.getX()] = 'X';
     }
 
     private void markSingleCrossed(Point p) {
-        System.out.println("MARKSINGLECROSSED");
         mazeArr[(int) p.getY()][(int) p.getX()] = 'O';
     }
 
     private void handleNonJunction(Point p) {
-        System.out.println("not at a junction");
         if (toFwd(p) == 'O') {
-            System.out.println("there's an O in front of me");
             moveFwd(p);
             markDoubleCrossed(p);
-            System.out.println("marked it as an X and moved to it");
         } else {
             if (toFwd(p) == ' ') {
-                System.out.println("there's a blank space in front of me");
                 moveFwd(p);
-                System.out.println("moved fwd once");
             } else if (toFwd(p) == '#') {
-                System.out.println("DEAD END");
                 turnRight();
                 turnRight();
-                System.out.println("turned around in place");
             }
         }
     }
 
     private boolean atJunction(Point p) {
-        System.out.println("in ATJUNCTION check");
         if (p.x == 0) {
-            System.out.println("ISJUNCTION p==start");
             return false;
         }
 
-        System.out.println("ISJUNCTION p!=star");
         int countPaths = 0;
         if (toFwd(p) != '#') {
             countPaths++;
@@ -158,15 +135,33 @@ public class Tremaux extends Algorithm {
 
     private void retraceSteps(Point p){
         
+        while (p.x != endCol) {
+            if(toFwd(p) == 'O'){
+                moveFwd(p);
+                steps.add('F');
+            }else if (toRight(p) == 'O'){
+                turnRight();
+                steps.add('R');
+            }else if (toLeft(p) == 'O'){
+                turnLeft();
+                steps.add('L');
+            }else if(toFwd(p) == ' '){
+                moveFwd(p);
+                steps.add('F');
+            }else if(toRight(p) == ' '){
+                turnRight();
+                steps.add('R');
+            }else if(toLeft(p) == ' '){
+                turnLeft();
+                steps.add('L');
+            }else{ 
+                turnRight();
+                steps.add('R');
+                turnRight();
+                steps.add('R');
+            }
+        }
+
     }
 
-    private void printMazeState() {
-        for (int i = 0; i < mazeArr.length; i++) {
-            for (int j = 0; j < mazeArr[i].length; j++) {
-                System.out.print(mazeArr[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
 }
